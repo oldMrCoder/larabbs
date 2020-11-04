79828dd7f43c25cc5fdb1425d9bac4fe835a148e
oldMrCoder <jiangziyi@yeah.net>
2020-11-04 22:04:36 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+// 《个人中心页面》相关路由
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
