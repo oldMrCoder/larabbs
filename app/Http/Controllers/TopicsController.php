a6717af7b85f255f44350995d931483d145be5a9
oldMrCoder <jiangziyi@yeah.net>
2020-11-09 19:49:44 +0800

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		// 预加载 user , category 两个数据表，修复 N+1 问题，即减少对数据库的查询次数
+		$topics = Topic::with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
